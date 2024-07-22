937a7c4b93b48e4f4267c0c862ed1a8f2d4b0dd3
Shivere <shawn.shivere@zeraki.co.ke>
2024-07-22 06:11:56 +0300

    Adding Google Authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, ImportProvidersSource } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
@@ -11,14 +11,19 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-40ecc","appId":"1:476540610007:web:5d61063827537fd5001792","storageBucket":"friendlychat-40ecc.appspot.com","locationId":"us-central","apiKey":"AIzaSyDY071R0PhYSWNpCuTqH6S-mDNvBIV090A","authDomain":"friendlychat-40ecc.firebaseapp.com","messagingSenderId":"476540610007"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    // importProvidersFrom(
+    //   provideFirebaseApp(() => initializeApp(environment.firebase)),
+    //   provideFirestore(() => getFirestore()),
+    //   provideAuth(() => getAuth()),
+    //   provideFunctions(() => getFunctions()),
+    //   provideStorage(() => getStorage()),
+    //   provideMessaging(() => getMessaging())
+    // ),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage())
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
+    apiKey: 'IzaSyDY071R0PhYSWNpCuTqH6S-mDNvBIV090A',
+    authDomain: 'friendlychat-40ecc.firebaseapp.com',
     databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
+    projectId: 'friendlychat-40ecc',
+    storageBucket: 'friendlychat-40ecc.appspot.com',
+    messagingSenderId: '476540610007',
+    appId: '1:476540610007:web:5d61063827537fd5001792',
     measurementId: 'G-MEASUREMENT_ID',
+    locationId:"us-central",
   },
 };
