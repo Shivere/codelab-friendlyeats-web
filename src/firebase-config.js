3857298d5a9402d8b71ec15fff160ce888d8670f
Shivere <shawn.shivere@zeraki.co.ke>
2024-07-22 06:59:05 +0300

    Rules modification

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    apiKey: 'IzaSyDY071R0PhYSWNpCuTqH6S-mDNvBIV090A',
+    authDomain: 'friendlychat-40ecc.firebaseapp.com',
+    databaseURL: 'https://PROJECT_ID.firebaseio.com',
+    projectId: 'friendlychat-40ecc',
+    storageBucket: 'friendlychat-40ecc.appspot.com',
+    messagingSenderId: '476540610007',
+    appId: '1:476540610007:web:5d61063827537fd5001792',
+    measurementId: 'G-MEASUREMENT_ID',
+    locationId:"us-central",
+  },
+};
